b5a02ca1b3368b7e33effc34a8776392dd77b568
Charlie Vieillard <charlie.vieillard@pxlwidgets.com>
2022-03-24 13:47:08 +0100

    Setup Vuetify 3.0.0-beta.0.

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,7 @@ import { WheelOfFortune } from './components'
 </script>
 
 <template>
-  <WheelOfFortune />
+  <v-app>
+    <WheelOfFortune />
+  </v-app>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
